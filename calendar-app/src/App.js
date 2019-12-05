5f203f9d82c709b2f7acaf480c5b207a77f48eae
William Lefebvre <williamlefebvre@MacBook-Air-de-William.local>
2019-12-05 18:18:28 +0100

    Import calendar

--- calendar-app/src/App.js
+++ calendar-app/src/App.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { createGlobalStyle } from "styled-components";
+import { StyledApp } from "./AppStyles";
+import { createCalendar } from "./helpers";
+import Hatch from "./Hatch";
 
-import './App.css';
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: center / cover url("./img/calendar_backdrop.jpg");
+    margin: 0;
+  }
+`;
 
 function App() {
+  const [hatches, setHatches] = useState([]);
+
+  useEffect(() => {
+    // Could use if statements instead off course
+    const calendar = localStorage.calendar
+      ? JSON.parse(localStorage.calendar)
+      : createCalendar();
+
+    setHatches(calendar);
+  }, []);
+
+  // Store calendar in localStorage
+  useEffect(() => {
+    // Could use if statements instead off course
+    hatches.length && localStorage.setItem("calendar", JSON.stringify(hatches));
+  }, [hatches]);
+
+  const handleFlipHatch = id => {
+    const updatedHatches = hatches.map(hatch =>
+      hatch.id === id ? { ...hatch, open: !hatch.open } : hatch
+    );
+    setHatches(updatedHatches);
+  };
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        {hatches.map(hatch => (
+          <Hatch
+            key={hatch.id}
+            hatchData={hatch}
+            handleClick={handleFlipHatch}
+          />
+        ))}
+      </StyledApp>
+    </>
   );
 }
 
