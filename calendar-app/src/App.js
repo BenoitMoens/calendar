516d8092a9675328d1626844a794b4269e962863
Benoît Moëns <benoit.moens@hotmail.com>
2019-12-05 21:32:21 +0100

    fusion

--- calendar-app/src/App.js
+++ calendar-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-//import Admin from ‘./components/Admin.js’
-//import Accueil from ‘./components/Accueil’
-//import ‘./App.css’;
+//import Admin from './components/Admin.js'
+//import Accueil from './components/Accueil'
+//import './App.css';
 import Calendar from './components/Calendar.js';
 function App() { 
   return (
