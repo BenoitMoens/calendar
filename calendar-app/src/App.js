a8981ef117caab5e0ca99784fc4f63115523ef7b
Benoît Moëns <benoit.moens@hotmail.com>
2019-12-05 15:33:04 +0100

    font

--- calendar-app/src/App.js
+++ calendar-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import './components/Accueil'
 import './App.css';
+import Accueil from './components/Accueil';
 
 function App() {
   return (
+    
     <div className="App">
-      
+      <Accueil />
     </div>
+    
   );
 }
 
---
+++ calendar-app/src/components/Accueil.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Accueil.css';
+
+const Accueil = () => {
+    return(
+        <div className="background">
+            
+            <div className="adminButton"><button className="sizeOfButtonAdmin">admin</button></div>
+            <div className="userButton"><button className="sizeOfButtonUser">Xmax Calendar</button></div>
+
+        </div>
+
+    );
+
+
+}
+
+export default Accueil;
