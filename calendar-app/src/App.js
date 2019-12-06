cc279641d3345a54c058894fb42668682ec0f2d5
Benoît Moëns <benoit.moens@hotmail.com>
2019-12-06 09:56:03 +0100

    lst version

--- calendar-app/src/App.js
+++ calendar-app/src/App.js
@@ -28,7 +28,7 @@ function App() {
     </div>
     
   </BrowserRouter>
-      <Admin />
+
 
     </div>
     
--- calendar-app/src/components/Accueil.js
+++ calendar-app/src/components/Accueil.js
@@ -22,14 +22,14 @@ const Accueil = () => {
                 </button>
             </div>
             
-            <div className="adminButton"><button className="sizeOfButtonAdmin">admin</button></div>
+            <div className="adminButton"></div>
             <iframe width="200" height="150" 
             src="https://www.youtube.com/embed/KQ-7O3fhAuQ" 
             frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
             allowfullscreen>
 
             </iframe>
-            <div className="userButton"><button className="sizeOfButtonUser">Xmax Calendar</button></div>
+            <div className="userButton"></div>
 
         </div>
 
